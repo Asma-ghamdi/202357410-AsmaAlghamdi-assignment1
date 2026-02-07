0f032c3e524d9429257db194c568d81d3832f1a5
Asma <asmaalghamdi95@gmail.com>
2026-02-07 16:36:23 +0300

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+// Get the HTML element where the greeting message will be displayed
 const greetingElement = document.getElementById("greeting");
 
+// Get the current hour from the system time
 const currentHour = new Date().getHours();
 
+// Variable to store the greeting message
 let greetingMessage;
 
+// Check the time and set the appropriate greeting
 if (currentHour < 12) {
     greetingMessage = "Good morning!";
 } else if (currentHour < 18) {
@@ -12,4 +16,5 @@ if (currentHour < 12) {
     greetingMessage = "Good evening!";
 }
 
+// Display the greeting message on the webpage
 greetingElement.textContent = greetingMessage;
