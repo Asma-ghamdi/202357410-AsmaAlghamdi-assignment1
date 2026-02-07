a5a119f5b29a7f72aeda6cc3cf289e3b2412b5c2
Asma <asmaalghamdi95@gmail.com>
2026-02-07 15:53:20 +0300

    fixing a file name

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+let greetingMessage;
+
+if (currentHour < 12) {
+    greetingMessage = "Good morning!";
+} else if (currentHour < 18) {
+    greetingMessage = "Good afternoon!";
+} else {
+    greetingMessage = "Good evening!";
+}
+
+greetingElement.textContent = greetingMessage;
